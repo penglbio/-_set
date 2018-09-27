4fb2bf2c904d05277672bb4efe3b41109184c8c8
penglbio <pengli@sibs.ac.cn>
2018-09-27 11:16:54 +0800

    update

--- linux_set_script.sh
+++ linux_set_script.sh
@@ -2,7 +2,8 @@
 step1:install sshserver
 step2:ssh-keygen -t sra
       ssh-copy-id lpeng@10.41.25.100
-step3:local set:psc='ssh lpeng@10.41.24.100 -Y'
+step3:local set:alias psc='ssh lpeng@10.41.25.100 -Y'
+		alias xfu='ssh xfu@10.41.9.210 -Y'
 step4:just type the 'psc' at the terminal
 ###bookmarks
 address:/home/galaxy/.mozilla/firefox/nvdi6sxn.default/bookmarkbackups/
@@ -11,6 +12,9 @@ cp the files in the directory to the new machine
 git config --global user.email "pengli@sibs.ac.cn"
 git config --global user.name "penglbio"
 echo "# linux_set_scripts" >> README.md
+git init
+git add README.md
 git commit -m "first commit"
-
+git remote add origin https://github.com/penglbio/linux_set_scripts.git
+git push -u origin master
 
